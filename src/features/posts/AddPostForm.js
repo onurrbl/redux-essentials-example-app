d9102853b4205d7d4f306d7cfc4e0bab6fb0df22
onurrbl <70076850+onurrbl@users.noreply.github.com>
2022-05-02 17:21:11 +0300

    finish async logic and thunks

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,33 +1,54 @@
 import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
+// import { postAdded } from './postsSlice'
 
-import { postAdded } from './postsSlice'
+import { addNewPost } from './postsSlice'
 
 export const AddPostForm = () => {
   const [title, setTitle] = useState('')
   const [content, setContent] = useState('')
   const [userId, setUserId] = useState('')
-  
+  const [addRequestStatus, setAddRequestStatus] = useState('idle')
+
   const dispatch = useDispatch()
 
-  const users = useSelector(state => state.users)
+  const users = useSelector((state) => state.users)
 
-  const onTitleChanged = e => setTitle(e.target.value)
-  const onContentChanged = e => setContent(e.target.value)
-  const onAuthorChanged = e => setUserId(e.target.value)
+  const onTitleChanged = (e) => setTitle(e.target.value)
+  const onContentChanged = (e) => setContent(e.target.value)
+  const onAuthorChanged = (e) => setUserId(e.target.value)
 
- const onSavePostClicked = () => {
-    if (title && content) {
-        dispatch(postAdded(title, content, userId))
-      setTitle('')
-      setContent('')
+  //  const onSavePostClicked = () => {
+  //     if (title && content) {
+  //         dispatch(postAdded(title, content, userId))
+  //       setTitle('')
+  //       setContent('')
+  //     }
+  //   }
+
+  //   const canSave = Boolean(title) && Boolean(content) && Boolean(userId)
+
+  const canSave =
+    [title, content, userId].every(Boolean) && addRequestStatus === 'idle'
+
+  const onSavePostClicked = async () => {
+    if (canSave) {
+      try {
+        setAddRequestStatus('pending')
+        await dispatch(addNewPost({ title, content, user: userId })).unwrap()
+        setTitle('')
+        setContent('')
+        setUserId('')
+      } catch (err) {
+        console.error('Failed to save the post: ', err)
+      } finally {
+        setAddRequestStatus('idle')
+      }
     }
   }
 
-  const canSave = Boolean(title) && Boolean(content) && Boolean(userId)
-
-  const usersOptions = users.map(user => (
+  const usersOptions = users.map((user) => (
     <option key={user.id} value={user.id}>
       {user.name}
     </option>
@@ -57,7 +78,7 @@ export const AddPostForm = () => {
           value={content}
           onChange={onContentChanged}
         />
-                <button type="button" onClick={onSavePostClicked} disabled={!canSave}>
+        <button type="button" onClick={onSavePostClicked} disabled={!canSave}>
           Save Post
         </button>
       </form>
--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,16 +1,15 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
 
+import { selectAllPosts, fetchPosts } from './postsSlice'
+
+import { Spinner } from '../../components/Spinner'
 import { PostAuthor } from './PostAuthor'
 import { TimeAgo } from './TimeAgo'
+import { ReactionButtons } from './ReactionButtons'
 
-export const PostsList = () => {
-  const posts = useSelector(state => state.posts)
-
-  const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
-  
-  const renderedPosts = orderedPosts.map(post => {
+const PostExcerpt = ({ post }) => {
     return (
       <article className="post-excerpt" key={post.id}>
         <h3>{post.title}</h3>
@@ -19,20 +18,72 @@ export const PostsList = () => {
           <TimeAgo timestamp={post.date} />
         </div>
         <p className="post-content">{post.content.substring(0, 100)}</p>
+  
+        <ReactionButtons post={post} />
         <Link to={`/posts/${post.id}`} className="button muted-button">
           View Post
         </Link>
-        <p>Asynclogic</p>
       </article>
-
-
     )
-  })
+  }
+
+
+export const PostsList = () => {
+    const dispatch = useDispatch()
+    const posts = useSelector(selectAllPosts)
+
+    const postStatus = useSelector(state => state.posts.status)
+    const error = useSelector(state => state.posts.error)
+
+    useEffect(() => {
+        if (postStatus === 'idle') {
+          dispatch(fetchPosts())
+        }
+      }, [postStatus, dispatch])
+
+
+      let content
+
+      if (postStatus === 'loading') {
+        content = <Spinner text="Loading..." />
+      } else if (postStatus === 'succeeded') {
+        // Sort posts in reverse chronological order by datetime string
+        const orderedPosts = posts
+          .slice()
+          .sort((a, b) => b.date.localeCompare(a.date))
+    
+        content = orderedPosts.map(post => (
+          <PostExcerpt key={post.id} post={post} />
+        ))
+      } else if (postStatus === 'failed') {
+        content = <div>{error}</div>
+      }
+
+
+//   const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+  
+//   const renderedPosts = orderedPosts.map(post => {
+//     return (
+//       <article className="post-excerpt" key={post.id}>
+//         <h3>{post.title}</h3>
+//         <div>
+//           <PostAuthor userId={post.user} />
+//           <TimeAgo timestamp={post.date} />
+//         </div>
+//         <p className="post-content">{post.content.substring(0, 100)}</p>
+//         <Link to={`/posts/${post.id}`} className="button muted-button">
+//           View Post
+//         </Link>
+//       </article>
+
+
+//     )
+//   })
 
   return (
     <section className="posts-list">
       <h2>Posts</h2>
-      {renderedPosts}
+      {content}
     </section>
   )
 }
